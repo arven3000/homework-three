fd8ac6aa0ac546b9ecb52cf8292a26d1c0c29251
Natali-Kremneva <van2gog6@gmail.com>
2022-01-31 20:18:36 +0300

    Корректировки домашней работы третьего урока

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -22,22 +22,27 @@ public class Main {
 
         int clientDeviceYear = 2015;
 
-        if (clientOS == 0 && clientDeviceYear >= 2015) {
+        if (clientOS == 0) {
 
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear >= 2015) {
 
-        } else if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
 
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
 
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+            }
         }
-        if (clientOS == 1 && clientDeviceYear >= 2015) {
+        if (clientOS == 1) {
 
-            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
 
-        } else if (clientOS == 1) {
+            } else {
 
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
         }
 
         /*Task 3*/
